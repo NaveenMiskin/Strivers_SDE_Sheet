85a3ca41a14418daa5c0871bef04e1e105878ee5
navee <naveenmiskin194@gmail.com>
2026-06-08 22:50:09 +0530

    Day 3

---
+++ arrays_part_2/P7_Rotate_matrix_90degree.java
@@ -0,0 +1,38 @@
+package arrays_part_2;
+
+public class P7_Rotate_matrix_90degree {
+    public void transpose(int[][] arr){
+        int m = arr.length;
+        int n = arr[0].length;
+
+        for(int i=0; i<m; i++){
+            for(int j=i+1; j<n; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+    }
+
+    public void reverse(int[][] arr){
+        int m = arr.length;
+
+        for(int i=0; i<m; i++){
+            int lp = 0;
+            int rp = m - 1;
+
+            while(lp < rp){
+                int temp = arr[i][lp];
+                arr[i][lp] = arr[i][rp];
+                arr[i][rp] = temp;
+                lp++;
+                rp--;
+            }
+        }
+    }
+
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reverse(matrix);
+    }
+}
