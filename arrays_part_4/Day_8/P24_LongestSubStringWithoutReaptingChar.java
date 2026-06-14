12e9b0a8b1ad65286a3e652ce9fda4c0ce8d0fcf
navee <naveenmiskin194@gmail.com>
2026-06-15 00:14:12 +0530

    Day 8

--- arrays_part_4/Day_8/P24_LongestSubStringWithoutReaptingChar.java
+++ arrays_part_4/Day_8/P24_LongestSubStringWithoutReaptingChar.java
@@ -5,52 +5,20 @@ import java.util.HashSet;
 
 public class P24_LongestSubStringWithoutReaptingChar {
     
-    // Brute Force 
-    // TC O(n logn + n)
-    // Sc (1)
-    public int longestConsecutive_BF(int[] nums) {
-        if(nums.length == 0) return 0;
-        Arrays.sort(nums);
-        int longestlength = 1;
-        int lastsmaller = Integer.MIN_VALUE;
-        int cnt = 0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]-1 == lastsmaller){
-                cnt++;
-                lastsmaller = nums[i];
+    // TC -> O(n)
+    // SC -> O(n)
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> hs = new HashSet<>();
+        int len = 0;
+        int l = 0;
+        for(int r=0; r<s.length(); r++) {
+            while(hs.contains(s.charAt(r))) {
+                hs.remove(s.charAt(l));
+                l++;
             }
-            else if(nums[i] != lastsmaller){
-                cnt = 1;
-                lastsmaller = nums[i];
-            }
-            longestlength = Math.max(longestlength, cnt);
+            hs.add(s.charAt(r));
+            len = Math.max(len, r - l + 1);
         }
-        return longestlength;
-    }
-
-    //optimal Solution
-    // TC O(n)
-    // SC O(n)
-    public int longestConsecutive(int[] nums) {
-        if(nums.length == 0) return 0;
-        int longest = 1;
-        HashSet<Integer> hs = new HashSet<>();
-
-        for(int i=0; i<nums.length; i++){
-            hs.add(nums[i]);
-        }
-
-        for(int ele : hs){
-            if(!hs.contains(ele-1)){
-                int cnt = 1;
-                int x = ele;
-                while(hs.contains(x + 1)){
-                    x += 1;
-                    cnt++;
-                }
-                longest = Math.max(longest, cnt);
-            }
-        }
-        return longest;
+        return len;
     }
 }
